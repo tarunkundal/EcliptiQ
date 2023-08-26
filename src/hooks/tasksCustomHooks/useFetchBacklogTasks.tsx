8aadbfa494c27822ca1ca553f13a3b84bcb30d2e
Tarun Chauhan <chauhan271singh@gmail.com>
2023-08-26 11:23:02 +0530

    Custom hooks for tasks setup

---
+++ src/hooks/tasksCustomHooks/useFetchBacklogTasks.tsx
@@ -0,0 +1,16 @@
+import { useAppSelector } from '../../app/store';
+
+const useFetchBacklogTasks = () => {
+	const selectedTeamId = useAppSelector((state) => state.teams.selectedTeamId);
+	const allTasks = useAppSelector((state) => state.tasks.tasks);
+
+	// fetching selected team tasks
+	const allTeamTasks = allTasks.filter(
+		(task) => task.team_id === selectedTeamId
+	);
+
+	const todoTasks = allTeamTasks.filter((task) => task.stage === 'backlog');
+	return { todoTasks };
+};
+
+export default useFetchBacklogTasks;
